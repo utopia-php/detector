09512d06b4b3a1a4acca2403ea9c22f03975d79e
Khushboo Verma <43381712+vermakhushboo@users.noreply.github.com>
2025-02-06 19:06:29 +0530

    Separate Strategy class

--- src/Detector/Runtime.php
+++ src/Detector/Runtime.php
@@ -5,30 +5,6 @@ namespace Utopia\Detector\Detector;
 use Utopia\Detector\Detection\Runtime as RuntimeDetection;
 use Utopia\Detector\Detector;
 
-class Strategy
-{
-    public const FILEMATCH = 'filematch';
-
-    public const EXTENSION = 'extension';
-
-    public const LANGUAGES = 'languages';
-
-    private string $value;
-
-    public function __construct(string $value)
-    {
-        if (! in_array($value, [self::FILEMATCH, self::EXTENSION, self::LANGUAGES])) {
-            throw new \Exception("Invalid strategy: {$value}");
-        }
-        $this->value = $value;
-    }
-
-    public function getValue(): string
-    {
-        return $this->value;
-    }
-}
-
 class Runtime extends Detector
 {
     /**
